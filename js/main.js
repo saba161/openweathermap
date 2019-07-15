aba4eb985b70a1dbd7ef22bc8e7572fcd350e016
kogo <saba.koguashvili1999@mail.ru>
2019-07-15 17:08:33 +0400

    json

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/*jslint browser:true */
+'use strict';
+
+var weatherConditions = new XMLHttpRequest();
+var weatherForecast = new XMLHttpRequest();
+var cObj;
+var fObj;
+
+// GET THE CONDITIONS
+weatherConditions.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=fb3abc5ee9cb003e2111862ae5964391&units=imperial', true);
+weatherConditions.responseType = 'text';
+weatherConditions.send(null);
+
+weatherConditions.onload = function() {
+    if (weatherConditions.status === 200){
+        cObj = JSON.parse(weatherConditions.responseText); 
+        console.log(cObj);
+        document.getElementById('location').innerHTML = cObj.name;
+        document.getElementById('weather').innerHTML = cObj.weather[0].description;
+        document.getElementById('temperature').innerHTML = cObj.main.temp;
+        document.getElementById('desc').innerHTML ="Wind speed" + cObj.wind.speed;
+
+    } //end if
+}; //end function
+
+
+
+
+
+
+
+
+
+
+// GET THE FORECARST
+//weatherForecast.open('', '', true);
+weatherForecast.responseType = 'text'; 
+//weatherForecast.send();
+
+weatherForecast.onload = function() {
+if (weatherForecast.status === 200){
+	fObj = JSON.parse(weatherForecast.responseText);
+	console.log(fObj);
+	
+} //end if
+}; //end function
+
+
